acc027a38479a9c40ce012486ae66d21552b258b
GBTNetwork <114756890+alexwambu@users.noreply.github.com>
2025-07-20 13:42:20 +0300

    bridgeL2.sol

--- bridgeL2.sol
+++ bridgeL2.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BridgeL2 {
+    address public admin;
+    mapping(address => uint256) public balances;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "Only admin");
+        balances[to] += amount;
+    }
+}
 
